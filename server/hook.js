d9689fb2578bad676dfad9dc71b290d25611c245
Kain Winterheart <kain@astaroth.dev.autumncoffee.com>
2013-10-14 08:32:42 +0400

    will it be ok?

--- server/hook.js
+++ server/hook.js
@@ -1,14 +1,14 @@
 // create a gith server on port 700
-var gith = require('gith').create( 7000 );
+var gith = require('gith').create( 31338 );
 
 gith({
-  repo: 'ddewaele/node-wheat-blog-template'
+  repo: 'kainwinterheart/acblog'
 }).on( 'all', function( payload ) {
 	console.log( 'Post-receive form Github');
 	//console.log("Payload = " + JSON.stringify(payload));
 
 	// Execute a git fetch on the bare repo.
-	var gitRepoPath = "/home/ubuntu/node/node-wheat-blog-template.git";
+	var gitRepoPath = "/home/kain/acblog.git";
 	var gitCommand = "git --git-dir " + gitRepoPath + " fetch origin master:master";
 
 	var sys = require('sys')
